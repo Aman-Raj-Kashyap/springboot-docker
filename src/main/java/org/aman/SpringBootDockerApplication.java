139bd46f6eb71d707f1666e6d76b7fffce18ce11
Aman-Raj-Kashyap <amankashyap701@gmail.com>
2023-10-30 21:05:54 +0530

    Adding files

---
+++ src/main/java/org/aman/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package org.aman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@GetMapping("/message")
+	public String welcome() {
+		return "Welcome to Aman's world!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
